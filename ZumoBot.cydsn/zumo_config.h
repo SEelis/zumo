56755eec1cbc46646812b8bb043bc128ded4a18a
SEelis <e.fantasyfinland@gmail.com>
2019-01-21 10:39:48 +0200

    Update zumo_config.h

--- ZumoBot.cydsn/zumo_config.h
+++ ZumoBot.cydsn/zumo_config.h
@@ -2,15 +2,15 @@
 #define ZUMO_CONFIG_H_
 
 /* Change the following definition to 0 to disable MQTT and to 1 to enable it  */
-#define START_MQTT 1    
+#define START_MQTT 0    
 
 /* The following definitions must all be in double quotes */
-#define NETWORK_SSID "8zumo"
+#define NETWORK_SSID //SSID here
 
-#define NETWORK_PASSWORD "Zumo 010"
+#define NETWORK_PASSWORD //password here
 
-#define MQTT_BROKER "192.168.1.3"  
+#define MQTT_BROKER //MQTT ip here 
     
-#define MQTT_CLIENT_ID "Zumo010"    
+#define MQTT_CLIENT_ID //MQTT id here   
 
 #endif
